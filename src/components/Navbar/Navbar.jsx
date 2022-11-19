be0cbe4cee482e92da1e15429bcbe0c0040cebe8
fahreza raharjo <fahrezaraharjo92@gmail.com>
2022-11-19 23:11:37 +0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,14 @@ const Navbar = () => {
         <div>
             <img src={images.logo} alt="logo" />
         </div>
+        <ul>
+          {['home', 'about', 'contact', 'work', 'skills', 'testimonial'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+          ))}
+        </ul>
     </nav>
     
   )
