6d45f02145d904e7756bbf30ba95a805817b7ab2
fahreza raharjo <fahrezaraharjo92@gmail.com>
2022-11-23 15:06:56 +0700

    done project

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { urlFor, client } from '../../client';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 
 
@@ -38,5 +38,8 @@ const About = () => {
     </>
   )
 }
-
-export default AppWrap(About, 'about')
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
